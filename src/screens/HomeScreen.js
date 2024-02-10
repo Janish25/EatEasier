cfa8f6f31fdc18d6825065e6e691aa890b7d3bd2
Janish Kumar U <125370708+Janish25@users.noreply.github.com>
2024-02-10 15:39:24 +0530

    Add files via upload

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, Card, CardActionArea, Typography } from '@material-ui/core';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+import { useStyles } from '../styles';
+import Logo from '../components/Logo';
+export default function HomeScreen(props) {
+  const styles = useStyles();
+  return (
+    <Card>
+      <CardActionArea onClick={() => props.history.push('/choose')}>
+        <Box className={[styles.root, styles.red]}>
+          <Box className={[styles.main, styles.center]}>
+            <Typography variant="h6" component="h6">
+              A Better Way!
+            </Typography>
+            <Typography variant="h1" component="h1" className={styles.bold}>
+              Eat <br />
+             
+              Easier
+            </Typography>
+            <TouchAppIcon fontSize="large"></TouchAppIcon>
+          </Box>
+          <Box className={[styles.center, styles.green]}>
+            <Logo large />
+            <Typography variant="h5" component="h5" className={styles.footer}>
+              Jobi
+            </Typography>
+          </Box>
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
+}
